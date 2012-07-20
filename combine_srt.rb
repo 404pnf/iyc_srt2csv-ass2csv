e2d81bae222b82721a34f2bfd23b601447ff1d79
404 <404@example.com>
2012-07-20 17:26:45 +0800

    增加新全局变量COMBINED来表示合并后文件名

--- combine_srt.rb
+++ combine_srt.rb
@@ -9,6 +9,7 @@ require 'fileutils'
 
 EN = ARGV[0]
 ZH = ARGV[1]
+COMBINED = EN.sub(/.(eng|chs).srt/, '.combined.srt.csv')
 OUTPUT = 'srt_csv'
 
 def splitfile(file)
@@ -35,22 +36,20 @@ def timeline_text_hash(arr)
 end
 def file_with_bom
   Dir.mkdir(OUTPUT) unless File.exist?(OUTPUT)
-#  oldname = ZH
-  #newname = oldname.sub('.chs.srt', 'combined.srt')
-  File.open("#{OUTPUT}/#{ZH}.csv", 'w')do |f|
+  File.open("#{OUTPUT}/#{COMBINED}", 'w')do |f|
     f.puts  "\uFEFF"
   end
 end
 def write_to_file(hash_en, hash_zh)
   # 悲剧，ruby的csv默认就是不quote值，浪费了半个小时才找到:force_quote用法
   # http://stackoverflow.com/questions/5831366/quote-all-fields-in-csv-output
-  CSV.open("srt_csv/#{ZH}.csv", 'a', {:force_quotes=>true}) do |csv| # append mode
-    hash_en.keys.each do |key|
-      zh = hash_zh[key]
-      en = hash_en[key]
-      csv << [zh,en]
+    CSV.open("#{OUTPUT}/#{COMBINED}", 'a', {:force_quotes=>true}) do |csv| # append mode
+      hash_en.keys.each do |key|
+        zh = hash_zh[key]
+        en = hash_en[key]
+        csv << [zh,en]
+      end
     end
-  end
 end
 def combine_src(en_srt, zh_srt)
   en_arr = splitfile(en_srt)
@@ -61,10 +60,6 @@ def combine_src(en_srt, zh_srt)
   write_to_file(hash_en, hash_zh)
 end
 
-
 combine_src(EN, ZH)
 
-#p splitfile(EN)
-#p splitfile(ZH)
-# hash_en = timeline_text_hash(splitfile(EN))
 

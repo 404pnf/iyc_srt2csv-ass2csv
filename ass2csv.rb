36097150d4139ebb99f6ac355844f22b55079f93
404 <404@example.com>
2012-07-30 10:53:22 +0800

    all new ass2csv.rb : use hash, the most general way

---
+++ ass2csv.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+require './lib/script2csv.rb'
+def r_ass2csv(input, output)
+  # expand input out path first, or else
+  # e.g input is 'user', output if 'other'
+  # /home/user/user will be replaced as /home/other/user, wrong!
+  # should be /home/user/other ya
+  # if we expand the path, that won't happen
+  input = File.expand_path $input
+  output = File.expand_path $output 
+  
+  Find.find(input) do |file|
+    next unless file =~ /ass$/i
+    next if File.directory?(file)
+    $inputfile = File.basename(file, 'ass') # $inputfile is filename. , with a dot
+    $inputpath = File.dirname(file)
+    $newpath = $inputpath.sub(input, output)
+    puts "正在处理： #{file}"
+    puts ""
+    ass2csv(file) # defined in lib/script2csv.rb
+  end
+  ending_msg
+end
+r_ass2csv($input, $output)
